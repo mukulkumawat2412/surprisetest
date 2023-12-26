cf5c3e8bc216473121fd1ebaa734407c1dd67d8b
mukulkumawat2412 <154887138+mukulkumawat2412@users.noreply.github.com>
2023-12-26 16:33:41 +0530

    Add files via upload

---
+++ surprise test/js/index.js
@@ -0,0 +1,117 @@
+
+
+// Ans (3)
+
+// function hide(){
+
+//       document.getElementById("btn").style.display = "none"
+
+// }
+
+
+// Ans (4) 
+
+//  let  arrowFun = (a,b)=>{
+
+
+//     let c = a*b
+//     document.getElementById("arrowFun").innerHTML = c
+
+
+//  }
+
+//  arrowFun(7,5)
+
+        
+
+
+//  Ans(5)
+//    let array = [12,18,45,23,98,76,56,90,84,68]
+
+
+//   let newArray =  array.filter((value)=>{
+//     return value>50
+//   })
+        
+
+// console.log(newArray)
+
+
+//  Ans(6)
+
+//     function myFUn(){
+
+//         let x = "ha ha ha iam a function"
+//         console.log(x)
+
+//     }
+
+//     setTimeout(myFUn,5000)
+
+
+
+
+// ans(7)
+
+
+//  function Fname(){
+
+//     let y = "you can repeat anytime"
+//     console.log(y)
+
+//  }
+
+//  setInterval(Fname,2000)
+
+
+
+
+//  Ans (8)
+
+//  function showPass(){
+//     let password = document.getElementById("fpass")
+
+ 
+
+
+
+//  if(password.type=="password"){
+
+//     password.type = "text"
+
+
+
+//  }
+//  else{
+//      password.type = "password"
+//  }
+
+//  }
+
+
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
